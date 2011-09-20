eda38c5f20ab159ffb54887be13b34b29aeb4811
willsam100 <willsam100@gmail.com>
2011-09-20 05:08:45 +0000

    added static network class. This is now the central point to do all networking. This will mean that multiple screens can use the same network class and only needs to be updated in one place.

--- kimt-android/src/com/waikato/kimt/KeepingInMusicalTouchDisplayDataActivity.java
+++ kimt-android/src/com/waikato/kimt/KeepingInMusicalTouchDisplayDataActivity.java
@@ -58,45 +58,8 @@ public class KeepingInMusicalTouchDisplayDataActivity extends Activity{
 
 		//get the data in the background. This means that the thread with UI will not be blocked 
 		protected String doInBackground(String... fullAddress) {
-			StringBuilder dumpString = null;
-
-			URL url = null;
-
-			try {
-				// Point the URL object at the full address
-				url	= new URL(fullAddress[0]);
-
-				// Create a new input stream from the URL
-				InputStream input = url.openStream();
-
-				// Create an array of bytes for the input stream to
-				// be written to
-				byte[] b = new byte[1024];
-				int bytesRead;
-
-				// We'll dump the byte array to this string
-				dumpString = new StringBuilder(1024);
-
-				do {
-					// Keep reading the input stream, then write to the array of
-					// bytes while there's data to be read.
-					bytesRead = input.read(b);
-
-					// Loop through the byte array and append each character
-					// to the dump string
-					for (int i = 0; i < bytesRead; i++) {
-						dumpString.append((char)b[i]);
-					}
-				} while (bytesRead != -1);
-
-				input.close();
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			return dumpString.toString();
+			String result = Network.getData(fullAddress[0]);
+			return result;
 		}
 
 		//the result of the above method will call this method and pass in the result. 
