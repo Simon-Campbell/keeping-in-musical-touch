7c7bf253936cc3377110733c617365c5323edc95
simon.s.campbell <simon.s.campbell@gmail.com>
2011-09-20 05:03:42 +0000

    - Nothing special, just fleshing out the MusicSheet.java. Hopefully it won't break!

---
+++ kimt-android/src/com/waikato/kimt/GreenstoneMusicLibrary.java
@@ -0,0 +1,29 @@
+package com.waikato.kimt;
+
+public class GreenstoneMusicLibrary implements MusicLibrary {
+
+	@Override
+	public void connect(String uri) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setCurrentSheet(String sheetID) {
+		// TODO Auto-generated method stub 
+
+	}
+
+	@Override
+	public MusicSheet getCurrentSheet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MusicSheet find(String searchTerm, SearchMode sm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
