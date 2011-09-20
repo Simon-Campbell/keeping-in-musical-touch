63cb83725804846525760f1d70875f8123c9b3d0
simon.s.campbell <simon.s.campbell@gmail.com>
2011-09-20 03:00:10 +0000

    - Reverted my previous change.

---
+++ kimt-android/src/com/waikato/kimt/MusicSheet.java
@@ -0,0 +1,17 @@
+package com.waikato.kimt;
+
+public class MusicSheet {
+	private String sheetID;
+	private GreenstoneMusicLibrary owner;
+	
+	private String title;
+	private String author;
+	
+	private int	pages;
+	private int currentPage;
+	
+	public MusicSheet(GreenstoneMusicLibrary owner, String sheetID) {
+		this.owner	= owner;
+		this.sheetID= sheetID;
+	}
+}
