44d906d1158ca976df66fcc8964c1120718a004d
willsam100 <willsam100@gmail.com>
2011-09-20 06:28:31 +0000

    Begin shift to greenstone UI, webview now loads the link but still needs more work.

--- kimt-android/src/com/waikato/kimt/KeepingInMusicalTouchActivity.java
+++ kimt-android/src/com/waikato/kimt/KeepingInMusicalTouchActivity.java
@@ -10,94 +10,95 @@ import android.view.View.OnClickListener;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class KeepingInMusicalTouchActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);    
-        this.setContentView(R.layout.main);
-        
-        
-        // Manually set up the onClick event for the show button,
-        // we can do this through main.xml also.
-        Button btnShow = (Button) findViewById(R.id.btnShow);
-        Button btnLoad = (Button) findViewById(R.id.btnLoad);
-        
-        //a comment as to not delete this file
-        
-        // Create an click listener and make it run our custom
-        // method elsewhere.
-        btnShow.setOnClickListener(new View.OnClickListener() {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);    
+		this.setContentView(R.layout.main);
+
+
+		//UI Buttons
+		Button btnShow = (Button) findViewById(R.id.btnShow);
+		Button btnLoad = (Button) findViewById(R.id.btnLoad);
+
+
+		//Listner for button
+		btnShow.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				
+
 				//get the data that the user entered
 				String fullAddress = btnShow_onClick(v);
-				
+
 				//package the data to that it can be sent to next activity
 				Bundle bundle = new Bundle();
 				bundle.putString("url", fullAddress);
-				
+
 				//call the next activity (the other view) and send the data to it
 				Intent myIntent = new Intent(v.getContext(), KeepingInMusicalTouchDisplayDataActivity.class); //creating
 				myIntent.putExtras(bundle); //data
 				startActivityForResult(myIntent, 0); //starting
-                
-				
+
+
 			}
 		});
-        
-        btnLoad.setOnClickListener(new View.OnClickListener() {
-        	
-        	@Override
-        	public void onClick(View v) {
-        		
-        		//get the url from the screen
-        		String fullAddress = btnShow_onClick(v);
-        		
-        		//display the url in the webview
-        		WebView myWebView = (WebView) findViewById(R.id.myWebView);
-        		myWebView.getSettings().setJavaScriptEnabled(true);
-        		myWebView.getSettings().setDefaultFontSize(19);
-        		String data = Network.getData(fullAddress);
-        		myWebView.loadData(data, "", "");
-        		myWebView.getSettings().setSupportZoom(true);
-        	}
-        });      
-    }
-    
-    /**
-     * The click event handler for the show button.
-     * @param view
-     */
-    
-    public String btnShow_onClick(View v)
-    {
-    	// Find the edit box for address and port, then assign them to
-    	// these local variables.
-    	EditText editAddress= (EditText) findViewById(R.id.editTextAddress);
-    	EditText editPort = (EditText) findViewById(R.id.editTextPort);
 
-    	// Get an integer representation of the port number:
-    	//	This is not required now but if we wish to send binary
-    	//	data via sockets it will be.
-    	@SuppressWarnings("unused")
-		int port = Integer.valueOf(editPort.getText().toString());
-    	
-    	// Get the address that has been entered into the address textbox
-    	String address	= editAddress.getText().toString();
-    	
-    	// Put the full address together so we can open it via URL()
-    	String fullAddress;
-    	
-    	if (!address.startsWith("http://"))
-    		fullAddress = "http://" + address;
-    	else
-    		fullAddress = address;
-    	
-    	return fullAddress;
-    }
-    
+		//Listner for button
+		btnLoad.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				//get the url from the screen
+				String fullAddress = btnShow_onClick(v);
+
+				//display the url in the webview
+				WebView myWebView = (WebView) findViewById(R.id.myWebView);
+				myWebView.getSettings().setJavaScriptEnabled(true);
+				myWebView.getSettings().setDefaultFontSize(19);
+				String data = Network.getData(fullAddress);
+				//        		myWebView.loadData(data, "text/html", "utf-8");
+				myWebView.loadUrl("http://www.nzdl.org/greenstone3-nema/dev;jsessionid=08C1CB94BDBF8322F72548075D809910?a=d&amp;ed=1&amp;book=off&amp;c=musical-touch&amp;d=HASH0151f62687a74edac75640ee&amp;excerptid=gs-document-text&amp;view=simple&amp;pagePrefix=dvorak-als-die-alte-mutter-a4&amp;pageSuffix=.png&amp;numPages=3&amp;pageWidth=426&amp;pageHeight=603&amp;scaleFactor=0.7161520190023754&amp;o=xml");
+				myWebView.getSettings().setSupportZoom(true);
+
+			}
+		});      
+	}
+
+	/**
+	 * The click event handler for the show button.
+	 * @param view
+	 */
+
+	public String btnShow_onClick(View v)
+	{
+		// Find the edit box for address and port, then assign them to
+		// these local variables.
+		TextView editAddress= (TextView) findViewById(R.id.editTextAddress);
+		//    	EditText editPort = (EditText) findViewById(R.id.editTextPort);
+
+		// Get an integer representation of the port number:
+		//	This is not required now but if we wish to send binary
+		//	data via sockets it will be.
+		//    	@SuppressWarnings("unused")
+		//		int port = Integer.valueOf(editPort.getText().toString());
+
+		// Get the address that has been entered into the address textbox
+		String address	= editAddress.getText().toString();
+
+		// Put the full address together so we can open it via URL()
+		String fullAddress;
+
+		if (!address.startsWith("http://"))
+			fullAddress = "http://" + address;
+		else
+			fullAddress = address;
+
+		return fullAddress;
+	}
+
 }
