29f8f128a32905edf28cb9d6cda6b8bfddf8a68e
simon.s.campbell <simon.s.campbell@gmail.com>
2011-09-22 13:00:38 +0000

    - Created a login view, made it the default view. You set the location of the Greenstone server (or Greg's server when that's done). We can add group name + passwords here to authenticate as we get further through. - Removed a whole bunch of debugging crap and tidied some bits and pieces up. There have been no major changes that need to be noted as far as I'm aware.

--- kimt-android/gen/com/waikato/kimt/R.java
+++ kimt-android/gen/com/waikato/kimt/R.java
@@ -14,19 +14,27 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int btnBack=0x7f050005;
-        public static final int btnLoad=0x7f050007;
-        public static final int btnShow=0x7f050006;
+        public static final int autoCompleteTextViewLibraryLocation=0x7f050009;
+        public static final int btnBack=0x7f05000c;
+        public static final int btnLoad=0x7f05000e;
+        public static final int btnSetLibrary=0x7f05000b;
+        public static final int btnShow=0x7f05000d;
         public static final int button_back=0x7f050001;
+        public static final int editText1=0x7f05000a;
         public static final int linearLayout1=0x7f050004;
-        public static final int myWebView=0x7f050008;
+        public static final int linearLayout2=0x7f050005;
+        public static final int linearLayout3=0x7f050008;
+        public static final int myWebView=0x7f05000f;
+        public static final int textView1=0x7f050006;
+        public static final int textView2=0x7f050007;
         public static final int textViewDump=0x7f050003;
         public static final int textViewFormatted=0x7f050002;
         public static final int textViewTitle=0x7f050000;
     }
     public static final class layout {
         public static final int gsdisplay=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int loginview=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
@@ -34,12 +42,17 @@ public final class R {
         public static final int btnBack=0x7f040009;
         public static final int btnForward=0x7f04000a;
         public static final int btnLoad=0x7f04000b;
+        public static final int btnSetLibrary=0x7f04000d;
         public static final int defaultAddress=0x7f040008;
+        public static final int defaultLibraryLocation=0x7f04000c;
         public static final int dumpString=0x7f040004;
         public static final int formattedString=0x7f040007;
         public static final int hello=0x7f040000;
         public static final int httpPort=0x7f040003;
+        public static final int librarySelect=0x7f040010;
         public static final int showBtn=0x7f040002;
         public static final int title=0x7f040006;
+        public static final int txtIntroduction=0x7f04000f;
+        public static final int txtLogin=0x7f04000e;
     }
 }
