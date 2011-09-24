2e171b55c9ff7de00f9b9acd50dffa9d69ce1433
simon.s.campbell@gmail.com <simon.s.campbell@gmail.com@65705e4d-d4fa-fb76-4a65-30c510ba754c>
2011-09-24 05:51:34 +0000

    - Added in threaded server socket reading and writing to Server.java. We can start sending dummy data from the Android application.

---
+++ trunk/kimt-server/src/com/waikato/kimt/server/KeepingInMusicalTouchServer.java
@@ -0,0 +1,13 @@
+package com.waikato.kimt.server;
+
+public class KeepingInMusicalTouchServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Server
+			s = new Server();
+	}
+
+}
