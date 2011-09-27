31231738cd8a9de1a548ade9f7f157be030efd23
willsam100 <willsam100@gmail.com>
2011-09-27 01:36:46 +0000

    impented listview into the main UI screen. This simply loads data from a string array. It will need to be connected to the networking setion to load the urls. further changes include altering the layout of the listview to diplay the images as well as click event handling.

--- kimt-android/src/com/waikato/kimt/KeepingInMusicalTouchActivity.java
+++ kimt-android/src/com/waikato/kimt/KeepingInMusicalTouchActivity.java
@@ -8,7 +8,10 @@ import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 
@@ -17,23 +20,23 @@ public class KeepingInMusicalTouchActivity extends Activity {
 		gml = null;
 
 	/** Called when the activity is first created. */
-	@Override
+	//@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		this.getWindow().requestFeature(Window.FEATURE_PROGRESS);
 		this.setContentView(R.layout.main);
 
-		Bundle
-			saved = this.getIntent().getExtras();
-		
-		if (saved != null)
-			gml = new GreenstoneMusicLibrary(saved.get("library_location") + "dev;jsessionid=08C1CB94BDBF8322F72548075D809910?a=d&ed=1&book=off&c=musical-touch&d=");
+		gml = new GreenstoneMusicLibrary(getString(R.string.defaultLibraryLocation) + "dev;jsessionid=08C1CB94BDBF8322F72548075D809910?a=d&ed=1&book=off&c=musical-touch&d=");
 
+		String[] items = {"Bass", "old school", "drums", "guitar"}; 
+		ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, items);
+		ListView listview = (ListView)findViewById(R.id.myListView);
+		listview.setAdapter(adapter);
+		
 		//UI Buttons
 		Button btnShow = (Button) findViewById(R.id.btnShow);
 		Button btnLoad = (Button) findViewById(R.id.btnLoad);
-		Button btnBack = (Button) findViewById(R.id.btnBack);
+		//Button btnBack = (Button) findViewById(R.id.btnBack);
 
 		//Listner for button
 		btnShow.setOnClickListener(new View.OnClickListener() {
@@ -52,8 +55,6 @@ public class KeepingInMusicalTouchActivity extends Activity {
 				Intent myIntent = new Intent(v.getContext(), KeepingInMusicalTouchDisplayDataActivity.class); //creating
 				myIntent.putExtras(bundle); //data
 				startActivityForResult(myIntent, 0); //starting
-
-
 			}
 		});
 
@@ -63,19 +64,9 @@ public class KeepingInMusicalTouchActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 
-				updateWebView();
+				//updateWebView();
 			}
 		});      
-		
-		btnBack.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent
-					loginIntent = new Intent(v.getContext(), KeepingInMusicalTouchLoginActivity.class);
-				startActivityForResult(loginIntent, 0);
-			}
-		});
 	}
 
 	/**
@@ -83,34 +74,34 @@ public class KeepingInMusicalTouchActivity extends Activity {
 	 * @param view
 	 */
 	
-	public void updateWebView() {
-		//display the url in the webview
-		WebView myWebView = (WebView) findViewById(R.id.myWebView);
-		
-		
-		final Activity activity = this;
-		
-		 myWebView.setWebChromeClient(new WebChromeClient() {
-		   public void onProgressChanged(WebView view, int progress) {
-		     // Activities and WebViews measure progress with different scales.
-		     // The progress meter will automatically disappear when we reach 100%
-		     activity.setProgress(progress * 1000);
-		   }
-		 });
-		 
-		 myWebView.setWebViewClient(new WebViewClient() {
-			   public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-			     Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
-			   }
-			 });
-		
-		myWebView.getSettings().setJavaScriptEnabled(true);
-		myWebView.getSettings().setDefaultFontSize(19);
-		myWebView.setVisibility(1);
-		
-		String string = "http://www.nzdl.org/greenstone3-nema/dev";
-		myWebView.loadUrl(string);
-		myWebView.getSettings().setSupportZoom(true);
-	}
+//	public void updateWebView() {
+//		//display the url in the webview
+//		WebView myWebView = (WebView) findViewById(R.id.myWebView);
+//		
+//		
+//		final Activity activity = this;
+//		
+//		 myWebView.setWebChromeClient(new WebChromeClient() {
+//		   public void onProgressChanged(WebView view, int progress) {
+//		     // Activities and WebViews measure progress with different scales.
+//		     // The progress meter will automatically disappear when we reach 100%
+//		     activity.setProgress(progress * 1000);
+//		   }
+//		 });
+//		 
+//		 myWebView.setWebViewClient(new WebViewClient() {
+//			   public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+//			     Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+//			   }
+//			 });
+//		
+//		myWebView.getSettings().setJavaScriptEnabled(true);
+//		myWebView.getSettings().setDefaultFontSize(19);
+//		myWebView.setVisibility(1);
+//		
+//		String string = "http://www.nzdl.org/greenstone3-nema/dev";
+//		myWebView.loadUrl(string);
+//		myWebView.getSettings().setSupportZoom(true);
+//	}
 
 }
