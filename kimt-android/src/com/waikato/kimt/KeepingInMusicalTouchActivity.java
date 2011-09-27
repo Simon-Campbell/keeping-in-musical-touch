e2184beb58e7c91355557042fa3e29874c22eabe
willsam100 <willsam100@gmail.com>
2011-09-27 01:42:01 +0000

    cleaned up code

--- kimt-android/src/com/waikato/kimt/KeepingInMusicalTouchActivity.java
+++ kimt-android/src/com/waikato/kimt/KeepingInMusicalTouchActivity.java
@@ -28,6 +28,9 @@ public class KeepingInMusicalTouchActivity extends Activity {
 
 		gml = new GreenstoneMusicLibrary(getString(R.string.defaultLibraryLocation) + "dev;jsessionid=08C1CB94BDBF8322F72548075D809910?a=d&ed=1&book=off&c=musical-touch&d=");
 
+		gml.getCurrentSheet();
+		
+		
 		String[] items = {"Bass", "old school", "drums", "guitar"}; 
 		ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, items);
 		ListView listview = (ListView)findViewById(R.id.myListView);
@@ -73,35 +76,5 @@ public class KeepingInMusicalTouchActivity extends Activity {
 	 * The click event handler for the show button.
 	 * @param view
 	 */
-	
-//	public void updateWebView() {
-//		//display the url in the webview
-//		WebView myWebView = (WebView) findViewById(R.id.myWebView);
-//		
-//		
-//		final Activity activity = this;
-//		
-//		 myWebView.setWebChromeClient(new WebChromeClient() {
-//		   public void onProgressChanged(WebView view, int progress) {
-//		     // Activities and WebViews measure progress with different scales.
-//		     // The progress meter will automatically disappear when we reach 100%
-//		     activity.setProgress(progress * 1000);
-//		   }
-//		 });
-//		 
-//		 myWebView.setWebViewClient(new WebViewClient() {
-//			   public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-//			     Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
-//			   }
-//			 });
-//		
-//		myWebView.getSettings().setJavaScriptEnabled(true);
-//		myWebView.getSettings().setDefaultFontSize(19);
-//		myWebView.setVisibility(1);
-//		
-//		String string = "http://www.nzdl.org/greenstone3-nema/dev";
-//		myWebView.loadUrl(string);
-//		myWebView.getSettings().setSupportZoom(true);
-//	}
 
 }
