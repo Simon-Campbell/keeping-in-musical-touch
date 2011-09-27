dacf0e0f077992624ba42e8c7f534d8e5666239c
willsam100@gmail.com <willsam100@gmail.com@65705e4d-d4fa-fb76-4a65-30c510ba754c>
2011-09-27 07:26:00 +0000

    minor changes

--- trunk/kimt-android/gen/com/waikato/kimt/R.java
+++ trunk/kimt-android/gen/com/waikato/kimt/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int autoCompleteTextViewLibraryLocation=0x7f050009;
@@ -24,7 +25,7 @@ public final class R {
         public static final int linearLayout1=0x7f050004;
         public static final int linearLayout2=0x7f050005;
         public static final int linearLayout3=0x7f050008;
-        public static final int myWebView=0x7f05000f;
+        public static final int myListView=0x7f05000f;
         public static final int textView1=0x7f050006;
         public static final int textView2=0x7f050007;
         public static final int textViewDump=0x7f050003;
@@ -33,8 +34,9 @@ public final class R {
     }
     public static final class layout {
         public static final int gsdisplay=0x7f030000;
-        public static final int loginview=0x7f030001;
-        public static final int main=0x7f030002;
+        public static final int listview=0x7f030001;
+        public static final int loginview=0x7f030002;
+        public static final int main=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
