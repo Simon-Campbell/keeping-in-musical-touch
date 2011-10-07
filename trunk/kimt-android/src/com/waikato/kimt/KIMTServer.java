1cfca77836cfa6f05d1a0d391dead731b0655a3b
simon.s.campbell@gmail.com <simon.s.campbell@gmail.com@65705e4d-d4fa-fb76-4a65-30c510ba754c>
2011-10-07 02:15:35 +0000

    - Added in basic java applet to test if it can run concurrently with android (yes, it can!)

---
+++ trunk/kimt-android/src/com/waikato/kimt/KIMTServer.java
@@ -0,0 +1,13 @@
+package com.waikato.kimt;
+
+public class KIMTServer {
+	public static final int serverPort = 51002;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Server has been started!");
+	}
+
+}
