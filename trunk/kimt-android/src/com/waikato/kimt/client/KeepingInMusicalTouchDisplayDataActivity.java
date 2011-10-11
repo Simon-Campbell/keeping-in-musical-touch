07b7fa151341f3745717e7c648c63fa5cf3b7128
simon.s.campbell@gmail.com <simon.s.campbell@gmail.com@65705e4d-d4fa-fb76-4a65-30c510ba754c>
2011-10-11 02:11:45 +0000

    - Added in networkable bitmap and made changes to activity

--- trunk/kimt-android/src/com/waikato/kimt/client/KeepingInMusicalTouchDisplayDataActivity.java
+++ trunk/kimt-android/src/com/waikato/kimt/client/KeepingInMusicalTouchDisplayDataActivity.java
@@ -1,13 +1,20 @@
 package com.waikato.kimt.client;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.waikato.kimt.R;
 import com.waikato.kimt.greenstone.MusicSheet;
+import com.waikato.kimt.greenstone.MusicSheet.MetaDataDownloadListener;
 
 public class KeepingInMusicalTouchDisplayDataActivity extends Activity {
 	
@@ -17,17 +24,29 @@ public class KeepingInMusicalTouchDisplayDataActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);    
 		this.setContentView(R.layout.gsdisplay);
-		
 
+		final ImageView imageSheet = (ImageView) findViewById(R.id.imageSheet);
+		final TextView formattedText = (TextView) findViewById(R.id.textViewFormatted);
+		
+		Bitmap sheetImage;
+
+	
 		//get the data from activity that called this one (in this case its the url full address from the main window)
 		Bundle bundle = this.getIntent().getExtras();
 		MusicSheet selectedSheet = (MusicSheet) bundle.getSerializable("selected_sheet");
 		
-		TextView tvFormatted = (TextView) findViewById(R.id.textViewFormatted);
-		tvFormatted.setText(selectedSheet.toString());
-	//	tvFormatted.setText(url);
+		formattedText.setText(selectedSheet.toString());
 		
-		ImageView image = (ImageView) findViewById(R.id.imageSheet);		
+		try { // nothing
+			sheetImage = BitmapFactory.decodeStream(new URL("http://upload.wikimedia.org/wikipedia/commons/0/02/NES_Super_Mario.png").openStream());
+			imageSheet.setImageBitmap(sheetImage);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
---
+++ trunk/kimt-android/src/com/waikato/kimt/networking/NetworkableBitmap.java
@@ -0,0 +1,9 @@
+package com.waikato.kimt.networking;
+
+import java.io.Serializable;
+
+import android.graphics.Bitmap;
+
+public class NetworkableBitmap implements Serializable {
+	Bitmap current ;
+}
