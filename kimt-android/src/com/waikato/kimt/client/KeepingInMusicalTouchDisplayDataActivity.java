0155c465a65196cc9d2691d6e8a94371ab7baf72
willsam100 <willsam100@gmail.com>
2011-10-18 02:56:16 +0000

    fixed image and is now full screen in potrait mode.

--- kimt-android/src/com/waikato/kimt/client/KeepingInMusicalTouchDisplayDataActivity.java
+++ kimt-android/src/com/waikato/kimt/client/KeepingInMusicalTouchDisplayDataActivity.java
@@ -3,10 +3,16 @@ package com.waikato.kimt.client;
 import android.app.Activity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.provider.ContactsContract.CommonDataKinds.Event;
 import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,18 +28,22 @@ import com.waikato.kimt.greenstone.MusicSheet;
 import com.waikato.kimt.sync.MusicalSyncClient;
 
 public class KeepingInMusicalTouchDisplayDataActivity extends Activity {
-	
-	
+
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		// Locate the SensorManager using Activity.getSystemService
+
+
+
 		super.onCreate(savedInstanceState);    
 		this.setContentView(R.layout.gsdisplay);
 
 		final ImageView imageSheet = (ImageView) findViewById(R.id.imageSheet);
 		final TextView formattedText = (TextView) findViewById(R.id.textViewFormatted);
 		//final ScrollView scrollView = (ScrollView) findViewById(R.id.imageScrollView);
-		
+
 		// Get the extra data bundled with this activities
 		// intent
 		Bundle bundle = this.getIntent().getExtras();
@@ -44,56 +54,24 @@ public class KeepingInMusicalTouchDisplayDataActivity extends Activity {
 			// Unserialize the MusicSheet that was sent in the bundle,
 			// this MusicSheet was the sheet that was selected earlier.
 			final MusicSheet selectedSheet = (MusicSheet) bundle.getSerializable("selected_sheet");
-			
+
 			formattedText.setText(selectedSheet.toString());
 			selectedSheet.setOnImageDownloadedListener(new MusicSheet.ImageDataDownloadListener() {
 				@Override
 				public void onImageDownloaded(MusicSheet ms) {
-					Bitmap bit = ms.getBitmap();
-					
-					
-					
-					
-					
-					 //imageSheet.setLayoutParams( new ViewGroup.LayoutParams(WindowManager.LayoutParams., WindowManager.LayoutParams.FILL_PARENT));
-					DisplayMetrics metrics = new DisplayMetrics();
-					getWindowManager().getDefaultDisplay().getMetrics(metrics);
-					
-					int newWidth, newHeight, oldHeight, oldWidth;
-					newWidth = metrics.widthPixels;
-					
-					oldHeight = bit.getHeight();
-			        oldWidth =  bit.getWidth();
-			        
-			        
-			        Double newHeightDouble =  Math.floor((oldHeight * newWidth) / oldWidth);
-			        newHeight = newHeightDouble.intValue();
-			        
-			        
-//			        imageSheet.setMinimumHeight(newHeight);
-//					imageSheet.setMinimumWidth(newWidth);
-//					imageSheet.setMaxHeight(newHeight);
-//					imageSheet.setMaxWidth(newWidth);
-			       
-			        
-			        
-			        
-			        
-			        imageSheet.setLayoutParams(new ScrollView.LayoutParams(newWidth, newHeight));
-			        
-					
+					imageSheet.setLayoutParams(new ScrollView.LayoutParams(1200, 2048));
 					imageSheet.setImageBitmap(ms.getBitmap());
-					imageSheet.setScaleType(ImageView.ScaleType.FIT_XY);
-
 				}
 			});
-			
+
 			// Set the bitmap from the internet ..
-			selectedSheet.setBitmapFromInternet(0, imageSheet.getHeight(), imageSheet.getWidth());
+			selectedSheet.setBitmapFromInternet(0, 800, 1280);
+
 		} else {
 			Toast.makeText(getApplicationContext(), "Waiting for the Conductor...", Toast.LENGTH_SHORT).show();
 		}
 	}
+
 
 	@Override
 	public void onBackPressed() {
